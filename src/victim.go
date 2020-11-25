9c9b74b01e0acfad60c01075f544a2b541c7c77d
halilkocaoz <halil.i.kocaoz@gmail.com>
2020-11-25 16:16:49 +0300

    Implemented starting.

---
+++ src/victim.go
@@ -0,0 +1,58 @@
+package main
+
+//femicidesinturkey.com/api/statistic/killer
+//femicidesinturkey.com/api/statistic/cause
+//femicidesinturkey.com/api/victim
+
+type FemicidesInTurkeyJsonResult struct {
+	Message     interface{} `json:"message"`
+	Information struct {
+		Count int         `json:"count"`
+		Pages int         `json:"pages"`
+		Next  string      `json:"next"`
+		Prev  interface{} `json:"prev"`
+	} `json:"information"`
+	Data []Victim
+}
+
+type Victim struct {
+	FullName string `json:"fullName"`
+	City     string `json:"city"`
+	Killer   struct {
+		Definition string `json:"definition"`
+		Status     string `json:"status"`
+	} `json:"killer"`
+	Methods []struct {
+		Method string `json:"method"`
+	} `json:"methods"`
+	Causes []struct {
+		Cause string `json:"cause"`
+	} `json:"causes"`
+	Adult             bool        `json:"adult"`
+	ProtectionRequest interface{} `json:"protectionRequest"`
+	Year              string      `json:"year"`
+	URL               string      `json:"url"`
+}
+
+func (v Victim) IsSuitToCreateSentence() bool {
+	return false
+}
+
+func (v Victim) CreateSentence() string {
+	if v.IsSuitToCreateSentence() {
+
+	}
+	return ""
+}
+
+func (v Victim) SelectByWhomStatement() string {
+	return ""
+}
+
+func (v Victim) SelectMethodStatement() string {
+	return ""
+}
+
+func (v Victim) SelectCauseStatement() string {
+	return ""
+}
